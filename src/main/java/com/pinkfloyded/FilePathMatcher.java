506961dfa497a0fcc796376ca3f6c8d4aa7c04e8
PinkFloyded <k.veneetreddy@gmail.com>
2018-08-25 01:49:52 +0530

    Add support for matching partial file names

--- src/main/java/com/pinkfloyded/FilePathMatcher.java
+++ src/main/java/com/pinkfloyded/FilePathMatcher.java
@@ -2,22 +2,42 @@ package com.pinkfloyded;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 final class FilePathMatcher {
     @Nonnull
-    static List<String> match(String str) {
+    static Stream<Path> match(@Nonnull String str) {
 
-        List<String> matches = new ArrayList<>();
+        List<Path> matches = new ArrayList<>();
 
-        File[] filesInDir = new File(str).listFiles();
+        File file = new File(str);
+        File[] filesInDir = file.listFiles();
         if (filesInDir != null) {
-            for (File file : filesInDir) {
-                matches.add(file.getAbsolutePath());
+            for (File f : filesInDir) {
+                matches.add(Paths.get(f.getAbsolutePath()));
             }
         }
 
-        return matches;
+        if (file.getParent() != null) {
+            String basename = getBaseName(str);
+            File[] matchesInParent = new File(new File(str).getParent())
+                    .listFiles((dir, name) -> name.startsWith(basename) && !name.equals(basename));
+
+            if (matchesInParent != null) {
+                for (File f : matchesInParent) {
+                    matches.add(Paths.get(f.getAbsolutePath()));
+                }
+            }
+        }
+
+        return matches.stream().sorted();
+    }
+
+    static String getBaseName(String path) {
+        return Paths.get(path).getFileName().toString();
     }
 }
--- src/test/java/com/pinkfloyded/FilePathMatcherTest.java
+++ src/test/java/com/pinkfloyded/FilePathMatcherTest.java
@@ -3,8 +3,10 @@ package com.pinkfloyded;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Arrays;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static com.pinkfloyded.FilePathMatcher.getBaseName;
 import static com.pinkfloyded.FilePathMatcher.match;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -16,16 +18,36 @@ public class FilePathMatcherTest {
 
     @Test
     public void emptyListForNonMatchingPath() {
-        assertThat(match("faskdfa;lksjfl;akjfk;ajsafj;aljad;").size(), is(0));
+        assertThat(match("faskdfa;lksjfl;akjfk;ajsafj;aljad;").count(), is(0L));
     }
 
     @Test
     public void matchesForValidDirectory() {
-        String[] expectedPaths = new String[]{
-                new File(FIXTURE_PATH, "container/abc.txt").getAbsolutePath(),
-                new File(FIXTURE_PATH, "container/jeep.txt").getAbsolutePath()
+        Path[] expectedPaths = {
+                Paths.get(new File(FIXTURE_PATH, "container/abc.txt").getAbsolutePath()),
+                Paths.get(new File(FIXTURE_PATH, "container/abcd.txt").getAbsolutePath()),
+                Paths.get(new File(FIXTURE_PATH, "container/jeep.txt").getAbsolutePath())
         };
-        assertThat(match(new File(FIXTURE_PATH, "container").getAbsolutePath()),
-                is(Arrays.asList(expectedPaths)));
+
+        assertThat(match(new File(FIXTURE_PATH, "container").getAbsolutePath()).toArray(), is(expectedPaths));
+    }
+
+
+    @Test
+    public void matchesPartiallyCompletedFiles() {
+        Path[] expectedPaths = new Path[]{
+                Paths.get(new File(FIXTURE_PATH, "container/abc.txt").getAbsolutePath()),
+                Paths.get(new File(FIXTURE_PATH, "container/abcd.txt").getAbsolutePath())
+        };
+        assertThat(match(new File(FIXTURE_PATH, "container/ab").getAbsolutePath()).toArray(), is(expectedPaths));
+    }
+
+    @Test
+    public void getsFileBaseName() {
+        assertThat(getBaseName("/var/data/test/testing.txt"), is("testing.txt"));
+    }
+    @Test
+    public void getsDirBaseName() {
+        assertThat(getBaseName("/var/data/test/testing"), is("testing"));
     }
 }
