54cca00abc55e1eaefe0abb101bb8d6fff49d9cb
PinkFloyded <k.veneetreddy@gmail.com>
2018-11-01 23:23:14 +0530

    1. Handle languages where the quotes are not part of the string PSI element. 2. Use TypedHandlerDelegate for auto-popup

--- src/main/java/com/pinkfloyded/FilePathCompletionContributor.java
+++ src/main/java/com/pinkfloyded/FilePathCompletionContributor.java
@@ -8,11 +8,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-
 
 public class FilePathCompletionContributor extends CompletionContributor {
-
 
     @Override
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
@@ -20,13 +17,11 @@ public class FilePathCompletionContributor extends CompletionContributor {
             VirtualFile parentDirectoryOfCurrentFile = parameters.getOriginalFile()
                                                                  .getVirtualFile()
                                                                  .getParent();
+
+            String queryString = getQueryString(parameters);
             String parentDirectoryOfCurrentFileStr = parentDirectoryOfCurrentFile == null ? "" :
                     parentDirectoryOfCurrentFile.getCanonicalPath();
 
-            int caretPositionInString = parameters.getOffset() - parameters.getPosition().getTextOffset();
-            String queryString = parameters.getPosition()
-                                           .getText()
-                                           .substring(1, caretPositionInString);
 
             FilePathMatcher.aggregateFilePaths(parentDirectoryOfCurrentFileStr, queryString)
                            .forEach(path -> result.withPrefixMatcher(queryString)
@@ -34,14 +29,37 @@ public class FilePathCompletionContributor extends CompletionContributor {
         }
     }
 
-    @Override
-    public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
-        return typeChar == File.separatorChar;
+    private static String getQueryString(CompletionParameters parameters) {
+        int caretPositionInString = parameters.getOffset() - parameters.getPosition().getTextOffset();
+        String queryString = parameters.getPosition()
+                                       .getText()
+                                       .substring(0, caretPositionInString);
+
+        if (queryString.startsWith("'") || queryString.startsWith("\"")) {
+            queryString = queryString.substring(1);
+        }
+
+        return queryString;
     }
 
     private static boolean isAStringLiteral(PsiElement element) {
         String text = element.getText();
-        return (text.startsWith("\"") && text.endsWith("\"")) || (text.startsWith("'") && text.endsWith("'"));
+        return (text.startsWith("\"") && text.endsWith("\"")) ||
+               (text.startsWith("'") && text.endsWith("'")) ||
+               getPreviousSiblingText(element).equals("\"") && getNextSiblingText(element).equals("\"") ||
+               getPreviousSiblingText(element).equals("\'") && getNextSiblingText(element).equals("\'");
+    }
+
+    private static String getPreviousSiblingText(PsiElement element) {
+        if (element.getPrevSibling() == null) return "";
+
+        return element.getPrevSibling().getText();
+    }
+
+    private static String getNextSiblingText(PsiElement element) {
+        if (element.getNextSibling() == null) return "";
+
+        return element.getNextSibling().getText();
     }
 }
 
